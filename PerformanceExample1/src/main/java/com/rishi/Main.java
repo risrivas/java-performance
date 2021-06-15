7104be2ae0a05f92070d4b417e07f6a351eb3102
Rishi Srivastava <rishisrivasta@gmail.com>
2021-06-16 06:40:09 +0800

    used code cache tuning

--- PerformanceExample1/src/main/java/com/rishi/Main.java
+++ PerformanceExample1/src/main/java/com/rishi/Main.java
@@ -1,11 +1,16 @@
 package com.rishi;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by rishi on 13/06/2021
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(20L);
+        System.out.println("starting the work now");
+
         PrimeNumbers primeNumbers = new PrimeNumbers();
         Integer max = Integer.parseInt(args[0]);
         primeNumbers.generateNumbers(max);
