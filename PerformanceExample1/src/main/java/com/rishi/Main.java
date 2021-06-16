17d349214b90bb77c02db6b8faed9b9ae4c82a09
Rishi Srivastava <rishisrivasta@gmail.com>
2021-06-17 06:28:32 +0800

    32-bit vs 64-bit JVM

--- PerformanceExample1/src/main/java/com/rishi/Main.java
+++ PerformanceExample1/src/main/java/com/rishi/Main.java
@@ -1,18 +1,21 @@
 package com.rishi;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * Created by rishi on 13/06/2021
  */
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        TimeUnit.SECONDS.sleep(20L);
-        System.out.println("starting the work now");
+        /*TimeUnit.SECONDS.sleep(20L);
+        System.out.println("starting the work now");*/
+
+        long start = System.currentTimeMillis();
 
         PrimeNumbers primeNumbers = new PrimeNumbers();
         Integer max = Integer.parseInt(args[0]);
         primeNumbers.generateNumbers(max);
+
+        long end = System.currentTimeMillis();
+        System.out.printf("%nElapsed time was %d ms%n", (end - start));
     }
 }
