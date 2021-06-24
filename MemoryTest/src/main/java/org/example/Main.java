2a2165ad0082409bb34bc160e290e65ffc579714
Rishi Srivastava <rishisrivasta@gmail.com>
2021-06-25 06:58:19 +0800

    solved memory test

--- MemoryTest/src/main/java/org/example/Main.java
+++ MemoryTest/src/main/java/org/example/Main.java
@@ -10,17 +10,25 @@ public class Main {
 	public void start() {
 		String last = "Z";
 		Container container = new Container();
-		container.setInitial("C");
+		container.setInitial("C"); // initial = C
 		another(container,last);
+
+		// B as copy reference initialHolder was marked to initial2 and
+		// lost after another() returned
 		System.out.print(container.getInitial());
+
+		// final output = AZB
+		// actual output = AZB
 	}
 	
 	public void another(Container initialHolder, String newInitial) {
-		newInitial.toLowerCase();
-		initialHolder.setInitial("B");
+		// initialHolder = C
+		// newInitial = Z
+		newInitial.toLowerCase(); // no effect
+		initialHolder.setInitial("B"); // B
 		Container initial2 = new Container();
-		initialHolder=initial2;
-		System.out.print(initialHolder.getInitial());
-		System.out.print(newInitial);
+		initialHolder=initial2; // A
+		System.out.print(initialHolder.getInitial()); // A
+		System.out.print(newInitial); // Z
 	}
 }
