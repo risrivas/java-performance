12183b8da1d2ea2e46b5028522dae748367fbde7
Rishi Srivastava <rishisrivasta@gmail.com>
2022-06-03 06:36:46 +0800

    completed memory exercise 2

--- JavaPerformanceMaster/src/main/java/memorytest2/Book.java
+++ JavaPerformanceMaster/src/main/java/memorytest2/Book.java
@@ -1,40 +1,47 @@
 package memorytest2;
 
 public class Book {
-	private int id;
-	private String title;
-	private String author;
-	private Price price;
-	
-	public int getId() {
-		return id;
-	}
+    private int id;
+    private String title;
+    private String author;
+    private Price price;
 
-	public String getTitle() {
-		return title;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getAuthor() {
-		return author;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public Book(int id, String title, String author, Double price) {
-		this.id = id;
-		this.title = title;
-		this.author = author;
-		this.price = new Price(price);
-	}
-	
-	public String toString() {
-		return title + " by " + author;
-	}
-	
-	public Price getPrice() {
-		return this.price;
-	}
-	
-	public void setPrice(Double price) {
-		this.price = new Price(price);
-	}
-	
+    public String getAuthor() {
+        return author;
+    }
+
+    public Book(int id, String title, String author, Double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = new Price(price);
+    }
+
+    public Book(Book other) {
+        this.id = other.getId();
+        this.title = other.getTitle();
+        this.author = other.getAuthor();
+        this.price = new Price(other.getPrice().getValue());
+    }
+
+    public String toString() {
+        return title + " by " + author;
+    }
+
+    public Price getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = new Price(price);
+    }
+
 }
